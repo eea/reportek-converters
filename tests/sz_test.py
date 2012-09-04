3d8171015ca5c675b8c5dc19899d122766846a2c
Alex Morega <alex@grep.ro>
2012-09-04 16:39:19 +0300

    list contents of 7z archive with unicode filenames

--- tests/sz_test.py
+++ tests/sz_test.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 import unittest
 from path import path
 
@@ -11,3 +12,10 @@ class SevenZipTest(unittest.TestCase):
         from convert import call
         result = call('list_7zip', str(sz_data / 'empty.7z'))
         self.assertIn('0 files, 0 folders', result)
+
+    def test_list_7zip_with_unicode_chars_returns_contents(self):
+        from convert import call
+        result = call('list_7zip', str(sz_data / 'twofiles.7z'))
+        self.assertIn('one.txt', result)
+        self.assertIn(u'tw\u0307\u0275.txt'.encode('utf-8'), result)
+        self.assertIn('2 files, 0 folders', result)
