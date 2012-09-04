516a0afd08097e2ef3050f58338869ca3da37c46
Alex Morega <alex@grep.ro>
2012-09-04 16:46:01 +0300

    rar test with unicode in filename

--- tests/rar_test.py
+++ tests/rar_test.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 import unittest
 from path import path
 
@@ -11,3 +12,9 @@ class RarConverterTest(unittest.TestCase):
         result = call('rar2list', str(rar_data / 'onefile.rar'))
         self.assertIn('fisier.txt', result)
         self.assertIn('04-09-12', result)
+
+    def test_rar2list_with_unicode_characters_returns_contents(self):
+        from convert import call
+        result = call('rar2list', str(rar_data / 'diacritics.rar'))
+        self.assertIn(u"director cu spații și diacritice".encode('utf-8'),
+                      result)
