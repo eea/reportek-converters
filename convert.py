4711beefa95d5fff3aa3974f0795cac7455f4c1b
Alex Morega <alex@grep.ro>
2012-09-04 16:44:14 +0300

    list contents of rar file

--- convert.py
+++ convert.py
@@ -5,5 +5,7 @@ import subprocess
 def call(converter, filename):
     if converter == 'list_7zip':
         return subprocess.check_output(['7za', 'l', filename])
+    elif converter == 'rar2list':
+        return subprocess.check_output(['unrar', 'l', filename])
     else:
         raise NotImplementedError
