adfdf847dc62856560fd6de2fe288bf580361288
mihai <mihai.dinca@eaudeweb.ro>
2013-01-17 12:25:49 +0200

    eliminate devnull

--- bin/gdal_gml_to_png.py
+++ bin/gdal_gml_to_png.py
@@ -3,7 +3,6 @@
 import subprocess
 import argparse
 import sys
-import os
 
 from tempfile import NamedTemporaryFile
 
@@ -17,31 +16,26 @@ if __name__ == '__main__':
         metavar='src-file',
         help='path to gml file')
     arguments = parser.parse_args()
-    with open(os.devnull, 'w') as fnull:
-        with NamedTemporaryFile() as gtiff_file:
-            rasterize_command_string = ('gdal_rasterize -q'
-                                            ' -burn 255 -burn 0 -burn 0'
-                                            ' -of GTiff -ts {width} {height}'
-                                            ' {src} {dst}')
-            rasterize_command = rasterize_command_string.format(**{
-                                    'width': constants.IMAGE_WIDTH,
-                                    'height': constants.IMAGE_HEIGHT,
-                                    'src': arguments.src_file,
-                                    'dst': gtiff_file.name})
+    with NamedTemporaryFile() as gtiff_file:
+        rasterize_command_string = ('gdal_rasterize -q'
+                                        ' -burn 255 -burn 0 -burn 0'
+                                        ' -of GTiff -ts {width} {height}'
+                                        ' {src} {dst}')
+        rasterize_command = rasterize_command_string.format(**{
+                                'width': constants.IMAGE_WIDTH,
+                                'height': constants.IMAGE_HEIGHT,
+                                'src': arguments.src_file,
+                                'dst': gtiff_file.name})
+        subprocess.call(
+                rasterize_command,
+                shell=True)
+        with NamedTemporaryFile(mode='w+') as png_file:
+            translate_command_string = ('gdal_translate -q -ot Byte'
+                                        ' -of PNG {0} {1}')
+            translate_command = translate_command_string.format(
+                                    gtiff_file.name,
+                                    png_file.name)
             subprocess.call(
-                    rasterize_command,
-                    stdout=fnull,
-                    stderr=fnull,
+                    translate_command,
                     shell=True)
-            with NamedTemporaryFile(mode='w+') as png_file:
-                translate_command_string = ('gdal_translate -q'
-                                            ' -of PNG {0} {1}')
-                translate_command = translate_command_string.format(
-                                        gtiff_file.name,
-                                        png_file.name)
-                subprocess.call(
-                        translate_command,
-                        stdout=fnull,
-                        stderr=fnull,
-                        shell=True)
-                sys.stdout.write(png_file.read())
+            sys.stdout.write(png_file.read())
