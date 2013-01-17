0571bd8028551f5996b12d80b7c65c6f3f80309e
mihai <mihai.dinca@eaudeweb.ro>
2013-01-17 17:09:06 +0200

    improve script to produce thumbnail on request

--- bin/gdal_gml_to_png.py
+++ bin/gdal_gml_to_png.py
@@ -18,8 +18,8 @@ if __name__ == '__main__':
     parser.add_argument(
         '-t',
         '--thumb',
-        metavar='generate a thumbnail',
-        help='generate thumbnail only')
+        help='generate thumbnail only',
+        action="store_true")
     arguments = parser.parse_args()
     with NamedTemporaryFile() as gtiff_file:
         rasterize_command_string = ('gdal_rasterize -q'
@@ -27,9 +27,14 @@ if __name__ == '__main__':
                                         ' -burn 255 -burn 0 -burn 0'
                                         ' -of GTiff -ts {width} {height}'
                                         ' {src} {dst}')
+        selected_width = constants.IMAGE_WIDTH
+        selected_height = constants.IMAGE_HEIGHT
+        if arguments.thumb:
+            selected_width = constants.IMAGE_WIDTH_TH
+            selected_height = constants.IMAGE_HEIGHT_TH
         rasterize_command = rasterize_command_string.format(**{
-                                'width': constants.IMAGE_WIDTH,
-                                'height': constants.IMAGE_HEIGHT,
+                                'width': selected_width,
+                                'height': selected_height,
                                 'src': arguments.src_file,
                                 'dst': gtiff_file.name})
         subprocess.call(
